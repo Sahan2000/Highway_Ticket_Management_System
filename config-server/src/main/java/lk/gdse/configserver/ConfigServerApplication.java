e4495d25f07a776c7cc4830c10908f35e8dc028c
Sahan2000 <udanasahan1@gmail.com>
2024-06-22 12:05:21 +0530

    add api gateway

--- config-server/src/main/java/lk/gdse/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/lk/gdse/configserver/ConfigServerApplication.java
@@ -2,8 +2,12 @@ package lk.gdse.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableConfigServer
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
