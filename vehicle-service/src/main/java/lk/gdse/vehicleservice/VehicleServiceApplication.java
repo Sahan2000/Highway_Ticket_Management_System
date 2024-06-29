5686f427d8b912f7f8422c41cfe3840a5cffed68
Sahan2000 <udanasahan1@gmail.com>
2024-06-29 11:58:37 +0530

    complete user crud operation

---
+++ vehicle-service/src/main/java/lk/gdse/vehicleservice/VehicleServiceApplication.java
@@ -0,0 +1,15 @@
+package lk.gdse.vehicleservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class VehicleServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VehicleServiceApplication.class, args);
+    }
+
+}
---
+++ vehicle-service/src/main/java/lk/gdse/vehicleservice/controller/VehicleController.java
@@ -0,0 +1,9 @@
+package lk.gdse.vehicleservice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/vehicle")
+public class VehicleController {
+}
