8ed692a2523c83766f63eb6da4410268dc04f3eb
Sahan2000 <udanasahan1@gmail.com>
2024-07-04 16:01:51 +0530

    change some files in payment service

--- payment-service/src/main/java/lk/gdse/paymentservice/dto/PaymentDTO.java
+++ payment-service/src/main/java/lk/gdse/paymentservice/dto/PaymentDTO.java
@@ -1,5 +1,7 @@
 package lk.gdse.paymentservice.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Null;
 import lk.gdse.paymentservice.ENUM.PaymentType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -11,8 +13,11 @@ import lombok.NoArgsConstructor;
 @Data
 @Builder
 public class PaymentDTO {
+    @Null(message = "Id generate by  program")
     private String paymentId;
+    @NotNull(message = "payment date cannot be a null")
     private String paymentDate;
+    @NotNull(message = "amount cannot be a null")
     private double amount;
     private String ticketId;
 }
